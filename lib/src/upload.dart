fbc17eca596b2abc87e472a14eda1782372199c9
Павел Снитко <brian57reg@gmail.com>
2020-04-28 23:40:57 +0300

    support upload files large than 10 megabytes

--- lib/src/upload.dart
+++ lib/src/upload.dart
@@ -7,9 +7,11 @@ import 'package:translit/translit.dart';
 import 'package:path/path.dart' as path;
 
 class UploadController extends ResourceController {
+
   UploadController(this.dir) {
     acceptedContentTypes = [ContentType("multipart", "form-data")];
   }
+
   final Directory dir;
 
   static String _gibberish(int size) {
@@ -39,7 +41,50 @@ class UploadController extends ResourceController {
     return fw;
   }
 
-  @Operation.post()
+  @override
+  FutureOr<RequestOrResponse>  willProcessRequest(Request req) async {
+    final raw = req.raw;
+    if (raw.method == 'POST') {
+      final res = <String, dynamic> {};
+      final boundary = raw.headers.contentType.parameters["boundary"];
+      final transformer = MimeMultipartTransformer(boundary);
+      final parts = await transformer.bind(raw).toList();
+
+      final filePart = parts.firstWhere((part)=>part.headers["content-disposition"].contains("filename"));
+    
+      final fileRegexp = RegExp(r'filename="(.*)"');
+      final nameRegexp = RegExp(r'[^A-Za-z0-9\.-]');
+      final fn = fileRegexp.firstMatch(filePart.headers['content-disposition']).group(1);
+      final fullfn = Translit().toTranslit(source: fn).replaceAll(nameRegexp, '-');
+      
+      final dotIndex = fullfn.lastIndexOf('.');
+      String ext = '';
+      if (dotIndex >= 0 ) {
+        ext = fullfn.substring(dotIndex);
+      }
+      final filename = fullfn.substring(0, dotIndex > 0 ? dotIndex : null);
+      final content = await filePart.toList();
+      final fw = _getFile(filename, ext);
+      if (fw != null) {
+        final sink = fw.openWrite();
+        content.forEach(sink.add);
+        await sink.close();
+        res['code'] = 0;
+        res['status'] = 'Файл успешно загружен';
+        res['oldName'] = fn;
+        res['newName'] = '/${dir.path}/${path.basename(fw.path)}';
+      } else {
+        res['status'] = 'Не удалось создать файл';
+        res['code'] = -1;
+      }
+      return Response(200, {}, res);
+    } else {
+      return Response(405, {}, null);// Method not allowed
+    }
+  }
+
+
+ /*  @Operation.post()
   Future<Response> postForm() async {
     final res = <String, dynamic> {};
 
@@ -78,5 +123,5 @@ class UploadController extends ResourceController {
       res['code'] = -1;
     }
     return Response(200, {}, res);
-  }
+  } */
 }
